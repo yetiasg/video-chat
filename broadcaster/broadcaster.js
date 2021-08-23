6c9f630c07a6f2fd788fabaa8fbb83ef36f6a787
yetiasg <yetiasg@gmail.com>
2021-08-23 14:37:36 +0200

    working video chat

--- broadcaster/broadcaster.js
+++ broadcaster/broadcaster.js
@@ -1,95 +1,107 @@
 const webSocket = new WebSocket('ws://localhost:3000');
 
-webSocket.onmessage = e => {
-  handleSignalingData(JSON.parse(e.data));
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
 }
 
-function handleSignalingData(data) {
-  switch (data.type){
-    case "answer":
-      peerConnection.setRemoteDescription(data.answer);
-      break;
-    case "candidate": 
-      peerConnection.addIceCandidate(data.candidate)
-      break;
-  }
-}
-
-let username = null;
-function sendUsername(){
-  username = document.querySelector('#username-input').value;
-  sendData({
-    type: 'store_user',
-    username
-  });
-}
-
-function sendData(data){
-  webSocket.send(JSON.stringify(data)); 
-}
-
-let localStream = null;
-let peerConnection = null;
-function startCall(){
-  document.querySelector('#video-call-container').style.display = "block";
-  navigator.getUserMedia({
-    video: {
-      frameRate: 30,
-      height: '100%',
-      aspectRatio: 16/9
-    },
-    audio: true
-  }, stream => {
-    localStream = stream;
-    document.querySelector('#local-video').srcObject = localStream;
-
-    let config = {
-      iceServers: [
-        {
-          'urls': ['stun.l.google.com:19302',
-            'stun1.l.google.com:19302',
-            'stun2.l.google.com:19302']
-        }
-      ]
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
     }
-
-    peerConnection = new RTCPeerConnection(config);
-    peerConnection.addStream(localStream);
-
-    peerConnection.onaddstream = e => {
-      document.querySelector('#remote-video').srcObject = e.stream;
-    }
-
-    peerConnection.onicecandidate = e => {
-      if( e.candidate == null) return;
-      sendData({
-        type: "store_candidate",
-        candidate: e.candidate
-      })
-    }
-
-    createAndSendOffer()
-  }, error => console.log(error));
 }
 
-function createAndSendOffer(){
-  peerConnection.createOffer( offer => {
+let username
+function sendUsername() {
+
+    username = document.getElementById("username-input").value
     sendData({
-      type: 'store_offer',
-      offer
+        type: "store_user"
     })
-    peerConnection.setLocatConnection(offer);
-  }, error => console.log(error));
 }
 
-let isAudio = true;
-function muteAudio(){
-  isAudio = !isAudio;
-  localStream.getAudioTracks()[0].enable = isAudio;
+function sendData(data) {
+    data.username = username
+    webSocket.send(JSON.stringify(data))
 }
 
-let isVideo = true;
-function muteVideo(){
-  isVideo = !isVideo;
-  localStream.getVideoTrack[0].enable = isVideo;
+
+let localStream
+let peerConn
+function startCall() {
+    document.getElementById("video-call-container")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video: {
+            frameRate: 24,
+            width: {
+                min: 480, ideal: 720, max: 1280
+            },
+            aspectRatio: 1.33333
+        },
+        audio: true
+    }, (stream) => {
+        localStream = stream
+        document.getElementById("local-video").srcObject = localStream
+
+        let configuration = {
+            iceServers: [
+                {
+                    "urls": ["stun:stun.l.google.com:19302", 
+                    "stun:stun1.l.google.com:19302", 
+                    "stun:stun2.l.google.com:19302"]
+                }
+            ]
+        }
+
+        peerConn = new RTCPeerConnection(configuration)
+        peerConn.addStream(localStream)
+
+        peerConn.onaddstream = (e) => {
+            document.getElementById("remote-video")
+            .srcObject = e.stream
+        }
+
+        peerConn.onicecandidate = ((e) => {
+            if (e.candidate == null)
+                return
+            sendData({
+                type: "store_candidate",
+                candidate: e.candidate
+            })
+        })
+
+        createAndSendOffer()
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+function createAndSendOffer() {
+    peerConn.createOffer((offer) => {
+        sendData({
+            type: "store_offer",
+            offer: offer
+        })
+
+        peerConn.setLocalDescription(offer)
+    }, (error) => {
+        console.log(error)
+    })
+}
+
+let isAudio = true
+function muteAudio() {
+    isAudio = !isAudio
+    localStream.getAudioTracks()[0].enabled = isAudio
+}
+
+let isVideo = true
+function muteVideo() {
+    isVideo = !isVideo
+    localStream.getVideoTracks()[0].enabled = isVideo
 }
---
+++ server/app.js
@@ -0,0 +1,95 @@
+const Socket = require('websocket').server;
+const http = require('http');
+
+const server = http.createServer((req, res) => {})
+
+
+
+const webSocket = new Socket({httpServer: server});
+let users = [];
+
+webSocket.on('request', (req) => {
+  const connection = req.accept();
+
+  connection.on('message', message => {
+    const data = JSON.parse(message.utf8Data);
+
+    const user = findUser(data.username)
+    console.log(data.type);
+
+    switch(data.type){
+      case 'store_user': 
+      if(user !=null) return;
+        const newUser = {
+          connection: connection,
+          username: data.username
+        }
+        users.push(newUser);
+        console.log(newUser.username);
+        break;
+      case 'store_offer': 
+        if(user == null) return;
+        user.offer = data.offer;
+        console.log('store_offer');
+        break;
+      case 'store_candidate':
+        if(user == null) return;
+        if(user.candidates == null) user.candidates = [];
+        user.candidates.push(data.candidate);
+        console.log('candidate');
+        break;
+      case 'send_answer':
+        if(user == null) return;
+        sendData({
+          type: 'answer',
+          answer: data.answer
+        }, user.connection);
+        console.log('send_answer');
+        break;
+      case 'send_candidate':
+        if(user == null) return;
+        sendData({
+          type: 'candidate',
+          candidate: data.candidate
+        }, user.connection);
+        console.log('send_candidate');
+        break;
+      case 'join_call': 
+        if(user == null) return;
+        sendData({
+          type: 'offer',
+          offer: user.offer
+        }, connection);
+        console.log('join_call');
+        user.candidates.forEach(candidate => {
+          sendData({
+            type: 'candidate',
+            candidate
+          }, connection)
+        })
+        break;
+    }
+  })
+  connection.on('close', (reason, description) => {
+    users.forEach(user => {
+      if(user.connection = connection){
+        users.splice(users.indexOf(user), 1);
+        return
+      }
+    })
+  })
+})
+
+function sendData(data, connection) {
+  connection.send(JSON.stringify(data));
+}
+
+
+function findUser(username) {
+    for (let i = 0;i < users.length;i++) {
+        if (users[i].username == username)
+            return users[i]
+    }
+}
+
+server.listen(3000, () => console.log("Listening on port: 3000"));
